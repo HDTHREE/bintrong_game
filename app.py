aa9893d7823751cfcb8e66ce795f6a19a0236304
HDTHREE <hd3nni4@gmail.com>
2025-11-13 22:41:37 -0500

    dotenv

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 
-app = FastAPI()
+_: bool = load_dotenv(r".dev.env")
+
+
+SGLANG_URL: str = os.environ.get("SGLANG_URL") or exit()
+
+
+app: FastAPI = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-
 
