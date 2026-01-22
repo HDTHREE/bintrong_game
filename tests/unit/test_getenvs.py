cd5168190068246d320c0b98aa09db34147f720a
HDTHREE <hd3nni4@gmail.com>
2026-01-22 17:23:30 -0500

    a

--- tests/unit/test_getenvs.py
+++ tests/unit/test_getenvs.py
@@ -1,5 +1,13 @@
+import logging
 import os
+
+import pytest
 from livetrivia.utils import getenvs
+
+
+def _clear():
+    os.environ["FOO"] = ""
+    del os.environ["FOO"]
 
 
 def test_getenvs():
@@ -33,7 +41,52 @@ def test_getenvs_with_type():
     assert KEY == 4444.0
 
 
-def test_getenvs_strict(): ...
+def test_getenvs_strict_exits():
+    _clear()
+
+    FOO: None = None
+    with pytest.raises(SystemExit, match=r"1"):
+        FOO = getenvs(strict=True)
+
+    assert FOO is None
 
 
-def test_getenvs_logger(): ...
+def test_getenvs_strict_pass():
+    _clear()
+
+    FOO: None = None
+    FOO = getenvs(strict=False)
+
+    assert FOO is None
+
+
+def test_getenvs_logger_print(capsys):
+    _clear()
+
+    FOO = getenvs(strict=False, logger=True)
+
+    assert "Cannot find env variable(s): FOO \n" == capsys.readouterr().out
+    assert FOO is None
+
+
+def test_getenvs_logger_false(capsys):
+    _clear()
+
+    FOO = getenvs(strict=False, logger=False)
+
+    assert "" == capsys.readouterr().out
+    assert FOO is None
+
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def test_getenvs_logger_object(caplog):
+    _clear()
+
+    caplog.set_level(logging.DEBUG)
+
+    FOO = getenvs(strict=False, logger=logger)
+
+    assert "Cannot find env variable(s): FOO" in caplog.text
+    assert FOO is None
