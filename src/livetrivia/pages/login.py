65777143fcd60fa3cb9544ef7bb6e08d7372616d
HDTHREE <hd3nni4@gmail.com>
2026-02-06 16:24:39 -0500

    formated login a bunch

--- src/livetrivia/pages/login.py
+++ src/livetrivia/pages/login.py
@@ -1,4 +1,3 @@
-import asyncio
 import aiohttp
 import dash
 import dash.exceptions as de
@@ -7,73 +6,70 @@ from livetrivia.models.user import LoginRequest
 from livetrivia.utils import getenvs, getmod
 from livetrivia.shared_components import token_store, user_store
 
+
 app: dash.Dash = dash.get_app()
+"""Reference to global dash object."""
 
 
 BACKEND_URL: str = getenvs(logger=app.logger)
 """URL to backend service."""
 
 
-email_input = dmc.TextInput(
-    placeholder="user@example.com",
-    label="Email",
-    size="md",
-    required=True,
-)
-
-
-password_input = dmc.PasswordInput(label="Password", size="md", required=True)
-
-
-confirm_input = dmc.PasswordInput(label="Confirm", size="md", required=True)
-
-
-login_button = dmc.Button("Login")
-
-
-new_button = dmc.Button(
-    "New?", id="new-button"
-)  # Needed for identification in callback.
-
-
-create_button = dmc.Button("Create")
-
-
-back_button = dmc.Button("Back")
-
-
-login_collapse = dmc.Collapse(
-    children=dmc.Flex([login_button, dmc.Space(w=10), new_button]),
-    keepMounted=True,
-    opened=True,
-)
-
-
-create_collapse = dmc.Collapse(
-    children=dmc.Stack(
-        [confirm_input, dmc.Flex([create_button, dmc.Space(w=10), back_button])],
-        w="100%",
-    ),
-    keepMounted=True,
-)
-
-
-login = dmc.Center(
-    dmc.Card(
-        w="60vw",
-        children=dmc.Fieldset(
-            children=[
-                email_input,
-                dmc.Space(h=10),
-                password_input,
-                dmc.Space(h=10),
-                login_collapse,
-                create_collapse,
-            ],
-            flex="column",
-        ),
+layout: dmc.AppShellMain = dmc.AppShellMain(
+    children=dmc.Center(
+        children=dmc.Card(
+            w="60vw",
+            children=dmc.Fieldset(
+                children=[
+                    email_input := dmc.TextInput(
+                        placeholder="user@example.com",
+                        label="Email",
+                        size="md",
+                        required=True,
+                    ),
+                    dmc.Space(h=10),
+                    password_input := dmc.PasswordInput(
+                        label="Password", size="md", required=True
+                    ),
+                    dmc.Space(h=10),
+                    login_collapse := dmc.Collapse(
+                        children=dmc.Flex(
+                            [
+                                login_button := dmc.Button(children="Login"),
+                                dmc.Space(w=10),
+                                new_button := dmc.Button(
+                                    children="New?", id="new-button"
+                                ),
+                            ]
+                        ),
+                        keepMounted=True,
+                        opened=True,
+                    ),
+                    create_collapse := dmc.Collapse(
+                        children=dmc.Stack(
+                            [
+                                confirm_input := dmc.PasswordInput(
+                                    label="Confirm", size="md", required=True
+                                ),
+                                dmc.Flex(
+                                    [
+                                        create_button := dmc.Button("Create"),
+                                        dmc.Space(w=10),
+                                        back_button := dmc.Button("Back"),
+                                    ]
+                                ),
+                            ],
+                            w="100%",
+                        ),
+                        keepMounted=True,
+                    ),
+                ],
+                flex="column",
+            ),
+        )
     )
 )
+"""Layout for login page. Embedded into `livetrivia._app` at `dash.page_container`."""
 
 
 @app.callback(
@@ -85,6 +81,7 @@ login = dmc.Center(
     prevent_initial_call=True,
 )
 async def on_login(_: int, email: str | None, password: str | None):
+    """Callback triggered when the user clicks login. Performs an API call on behalf of the user."""
     if not email or not password:
         raise de.PreventUpdate()
     user: LoginRequest = LoginRequest(email=email, password=password)
@@ -104,6 +101,7 @@ async def on_login(_: int, email: str | None, password: str | None):
     prevent_initial_call=True,
 )
 async def on_signup(_: int, email: str | None, password: str | None):
+    """Callback triggered when the user clicks sign up. Performs an API call."""
     if not email or not password:
         raise de.PreventUpdate()
     user: LoginRequest = LoginRequest(email=email, password=password)
@@ -114,9 +112,8 @@ async def on_signup(_: int, email: str | None, password: str | None):
         async with session.post(
             "api/sessions/login", json=user.model_dump()
         ) as login_response:
-            *_, token = await asyncio.gather(
-                session_response.json(), login_response.json()
-            )
+            (await session_response.json(),)
+            token = await login_response.json()
     return token, email
 
 
@@ -128,6 +125,8 @@ app.clientside_callback(
     dash.Input(back_button, "n_clicks"),
     prevent_initial_call=True,
 )
+"""Callback to toggle between the login and create account screens."""
+
 
 app.clientside_callback(
     dash.ClientsideFunction("login", "redirectToAccount"),
@@ -136,6 +135,7 @@ app.clientside_callback(
     dash.Input(token_store, "data"),
     dash.Input(user_store, "data"),
 )
+"""Callback to redirect the router to refresh. This triggers the callback `on_navigate` in `livetrivia._app` (i.e. navigate to `/accounts` after sign-[up|in])."""
 
 
 app.clientside_callback(
@@ -145,6 +145,7 @@ app.clientside_callback(
     dash.Input(password_input, "value"),
     prevent_initial_call=True,
 )
+"""Callback to update the disabled/enabled state of the login button."""
 
 
 app.clientside_callback(
@@ -155,9 +156,7 @@ app.clientside_callback(
     dash.Input(confirm_input, "value"),
     prevent_initial_call=True,
 )
-
-
-layout: dmc.AppShellMain = dmc.AppShellMain(children=login)
+"""Callback to update the disabled/enabled state of the create account button."""
 
 
 dash.register_page(
