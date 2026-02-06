0590c4e2dee43f2fd631321057c1784472d2d7da
HDTHREE <hd3nni4@gmail.com>
2026-02-06 16:55:38 -0500

    formated home a bunch

--- src/livetrivia/pages/home.py
+++ src/livetrivia/pages/home.py
@@ -2,8 +2,13 @@ import dash
 import dash_mantine_components as dmc
 from livetrivia.utils import getmod
 
+
+layout: dmc.AppShellMain = dmc.AppShellMain(children=dmc.Card())
+"""Layout for home page. Embedded into `livetrivia._app` at `dash.page_container`."""
+
+
 dash.register_page(
     getmod(__name__),
     path="/",
-    layout=dmc.AppShellMain(children=dmc.Card()),
+    layout=layout,
 )
