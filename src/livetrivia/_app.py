889a8d614dcc0f7ca312978f3e90ce9905b7c975
HDTHREE <hd3nnis4@gmail.com>
2026-04-05 15:04:25 -0400

    lint

--- src/livetrivia/_app.py
+++ src/livetrivia/_app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""Module that contains layout & navigation callbacks. Embeds a `dcc.Component` to act as page."""
 try:
     from dotenv import load_dotenv
 
     _: bool = load_dotenv(r".dev.dash.env")
 finally:
-    ...
+    pass
 import aiohttp
 import dash_iconify as di
 import dash
@@ -14,7 +15,7 @@ from livetrivia.utils import ClientsideFunctionType, assets_folder, getenvs, pag
 from livetrivia.shared_components import token_store, user_store, interval, url
 
 
-dash._dash_renderer._set_react_version("18.2.0")
+dash._dash_renderer._set_react_version("18.2.0") # pylint: disable=protected-access
 
 
 app: dash.Dash = dash.Dash(
@@ -53,15 +54,15 @@ app.layout = dmc.MantineProvider(
                                 icon="ic:round-connect-without-contact"
                             ),
                         ),
-                        login_link := dmc.NavLink(
-                            label=dmc.Text(dash.page_registry["login"]["name"], w=170),
-                            href=dash.page_registry["login"]["path"],
-                            leftSection=di.DashIconify(icon="ic:round-login"),
-                        ),
                         files_link := dmc.NavLink(
                             label=dmc.Text(dash.page_registry["files"]["name"], w=170),
                             href=dash.page_registry["files"]["path"],
                             leftSection=di.DashIconify(icon="ic:round-drive-file-move"),
+                        ),
+                        login_link := dmc.NavLink(
+                            label=dmc.Text(dash.page_registry["login"]["name"], w=170),
+                            href=dash.page_registry["login"]["path"],
+                            leftSection=di.DashIconify(icon="ic:round-login"),
                         ),
                         avatar_link := dmc.NavLink(
                             label=dmc.Text(
@@ -108,17 +109,17 @@ set_style: ClientsideFunctionType = app.clientside_callback(
     dash.State(user_store, "data"),
     prevent_initial_call=True,
 )
-def on_navigate(url: str | None, token: dict | None, user: str | None):
-    """Callback that triggers on navigation. Navigates users away from routes that require an account."""
+def on_navigate(navigation_url: str | None, token: dict | None, user: str | None):
+    """Callback that triggers on navigation. Navigates users away from protected routes."""
     session: bool = token and user
     real: set = {"/files", "/account", "/", "/login", "/join"}
     protected: set = {"/files", "/account"}
 
-    if not url or url not in real:
+    if not navigation_url or navigation_url not in real:
         return "/"
-    if url in protected and not session:
+    if navigation_url in protected and not session:
         return "/login"
-    if url == "/login" and session:
+    if navigation_url == "/login" and session:
         return "/account"
 
     raise de.PreventUpdate()
@@ -127,15 +128,15 @@ def on_navigate(url: str | None, token: dict | None, user: str | None):
 @app.callback(
     dash.Output(token_store, "data", allow_duplicate=True),
     dash.Output(user_store, "data", allow_duplicate=True),
-    dash.State(user_store, "id"),
+    dash.State(user_store, "value"),
     dash.State(interval, "id"),
     dash.Input(token_store, "data"),
     dash.Input(url, "pathname"),
     dash.Input(interval, "n_intervals"),
 )
-async def on_refresh(email: str | None, id: str, token: dict, *_: str | int | None):
+async def on_refresh(email: str | None, interval_id: str, token: dict, *_: str | int | None):
     """Callback that triggers when a user refreshes or navigates to ensure a valid session."""
-    if dash.ctx.triggered_id != id and token:
+    if dash.ctx.triggered_id != interval_id and token:
         raise de.PreventUpdate()
     async with aiohttp.ClientSession(BACKEND_URL) as session:
         # Get a guest session if no email.
@@ -150,7 +151,10 @@ async def on_refresh(email: str | None, id: str, token: dict, *_: str | int | No
             ) as session_response:
                 return await session_response.json(), dash.no_update
         finally:
-            return None, None
+            # Fr, why is this not reasonable? The behavior is easily explained and consistent.
+            # pylint standard is so ass; there no way to just have: "code might fail and idc" tf.
+            # Reasonable if you could do broad-caught-exception but no.
+            return None, None # pylint: disable=lost-exception, return-in-finally
 
 
 if __name__ == "__main__":
